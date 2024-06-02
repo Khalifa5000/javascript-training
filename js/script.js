a701342a9a45e089bd62aaa2e797450dc732e596
khalifa5000 <m.khalifa5000@gmail.com>
2024-06-02 23:38:18 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+document.write("<h1>Home page<h1/>");
+
+document.querySelector("h1").style.color = "blue";
+
+document.querySelector("h1").style.fontSize = "70px";
+
+// window.alert("welcome");
+console.log(
+  "hello from %cjs %cfile",
+  "color: red; font-size: 40px",
+  "color: blue; font-size: 50px"
+);
